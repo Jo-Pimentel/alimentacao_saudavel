2ea8686850f67e59061b5c50569f3a3c454f3f9b
Jo-Pimentel <joaopodolsky59@gmail.com>
2025-01-02 16:45:27 -0300

    Ajustes na calculadora de IMC.

--- js/script.js
+++ js/script.js
@@ -1,36 +1,48 @@
 function comparaImc(){
     let altura = parseFloat(document.getElementById("altura").value);
     let peso = parseFloat(document.getElementById("peso").value);
-    let imc = peso/(altura**2);
+    let imc = (peso/(altura**2)).toFixed(1);
     let idade = parseInt(document.getElementById("idade").value);
+    let situacao = document.getElementById("situacaoIlustrada").src;
 
-    if(idade<60){
+    try{
+        if(isNaN(altura) || isNaN(peso)) throw "Um dos valores inseridos não corresponde a um número.";
+    }
+    catch(erro){
+        document.getElementById("suaSituacao").innerHTML = (erro);
+    }
+
+    if(isNaN(idade)){
+        document.getElementById("suaSituacao").innerHTML = "Um dos valores inseridos não corresponde a um número.";
+    }
+
+    else if(idade<60){
         if(imc<18.5){
-            document.getElementById("suaSituacao").innerHTML = `Seu IMC é ${imc.toFixed(1)} e seu peso é baixo.`;
+            document.getElementById("suaSituacao").innerHTML = `Seu IMC é ${imc} e seu peso é baixo.`;
         }
         else if(imc>=18.5 && imc<25){
-            document.getElementById("suaSituacao").innerHTML = `Seu IMC é ${imc.toFixed(1)} e seu peso é adequado.`;
+            document.getElementById("suaSituacao").innerHTML = `Seu IMC é ${imc} e seu peso é adequado.`;
         }
         else if(imc>=25 && imc<35){
-            document.getElementById("suaSituacao").innerHTML = `Seu IMC é ${imc.toFixed(1)} e você está no primeiro nível de obesidade.`;
+            document.getElementById("suaSituacao").innerHTML = `Seu IMC é ${imc} e você está no primeiro nível de obesidade.`;
         }
         else if(imc>=35 && imc<40){
-            document.getElementById("suaSituacao").innerHTML = `Seu IMC é ${imc.toFixed(1)} e você está no segundo nível de obesidade.`;
+            document.getElementById("suaSituacao").innerHTML = `Seu IMC é ${imc} e você está no segundo nível de obesidade.`;
         }
         else{
-            document.getElementById("suaSituacao").innerHTML = `Seu IMC é ${imc.toFixed(1)} e você tem obesidade mórbida.`;
+            document.getElementById("suaSituacao").innerHTML = `Seu IMC é ${imc} e você tem obesidade mórbida.`;
         }
     }
 
     else{
         if(imc<22){
-            document.getElementById("suaSituacao").innerHTML = `Seu IMC é ${imc.toFixed(1)} e seu peso é baixo.`;
+            document.getElementById("suaSituacao").innerHTML = `Seu IMC é ${imc} e seu peso é baixo.`;
         }
         else if(imc>=22 && imc<27){
-            document.getElementById("suaSituacao").innerHTML = `Seu IMC é ${imc.toFixed(1)} e seu peso é adequado.`;
+            document.getElementById("suaSituacao").innerHTML = `Seu IMC é ${imc} e seu peso é adequado.`;
         }
         else{
-            document.getElementById("suaSituacao").innerHTML = `Seu IMC é ${imc.toFixed(1)} e você está sobrepeso.`;
+            document.getElementById("suaSituacao").innerHTML = `Seu IMC é ${imc} e você está sobrepeso.`;
         }
     }
 }
